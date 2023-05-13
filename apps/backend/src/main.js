54ee359c85111df0536a5395ac4334db9af12670
PaquitoSoft <paquitosoftware@gmail.com>
2023-05-13 13:05:22 +0200

    feat: implement hello query

--- apps/backend/src/main.js
+++ apps/backend/src/main.js
@@ -1,11 +1,40 @@
+import path from 'node:path';
+import { readFileSync } from 'node:fs';
+import { createServer } from 'node:http';
+
+import { createYoga, createSchema } from 'graphql-yoga';
 import { connectToDatabase } from './db/connection-manager';
+
+const typeDefs = readFileSync(path.join(__dirname, './schema.graphql'), 'utf8');
 
 async function bootstrap() {
 
-  // Connect to database
+  // 1. Connect to database
   await connectToDatabase(process.env.DATABASE_CONNECTION_URL);
   console.log('Connected to MongoDB!')
 
+  // 2. Create GraphQL server instance
+  const yoga = createYoga({
+    schema: createSchema({
+      typeDefs: typeDefs,
+      resolvers: {
+        Query: {
+          hello: (parent, params, context, info) => {
+            return `Hello ${params.name}!`;
+          }
+        }
+      }
+    })
+  });
+
+
+  // 3. Create HTTP server
+  const httpServer = createServer(yoga);
+
+  // 4. Start the server
+  httpServer.listen(4004, () => {
+    console.log('Server is running. Head to http://localhost:4004/graphql');
+  });
 
 }
 
